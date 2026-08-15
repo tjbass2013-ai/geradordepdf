377056f05e9f3b9760d2c9868c6a65c4132023d5
Thiago de Souza <tjbass2013@gmail.com>
2026-08-14 23:33:37 -0300

    remocao de trecho de codigo comentado desnecessariamente

--- raca.py
+++ raca.py
@@ -17,27 +17,7 @@ def raca():
 
         rac = racas.get(op)
         
-    #     if op == 1:
-    #         rac = "Humano"
-    #
-    #     elif op == 2:
-    #             rac ="Elfo"
-    #
-    #     elif op == 3:
-    #             rac = "Anao"
-    #
-    #     elif op == 4:
-    #             rac = "Halfling"
-    #
-    #     elif op == 5:
-    #             rac = "Tiefling"
-    #
-    #     elif op == 6:
-    #             rac = "Draconato"
-    #
-    #     elif op < 1 or op > 6:
-    #         rac = None
-    else:
+     else:
         rac = None
         
                 
